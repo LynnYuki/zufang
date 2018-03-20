05493843d7942928665eac86a17d62fcf364723c
LynnYuki <lynnyuki9527@163.com>
2018-03-20 09:49:12 +0800

    数据入库

--- zufang/spiders/ganji.py
+++ zufang/spiders/ganji.py
@@ -5,34 +5,38 @@ class GanjiSpider(scrapy.Spider):
 
 	name = "zufang"
 	#爬取域名
-	allowed_domain = ['cs.ganji.com']
+	allowed_domain = ['http://cs.ganji.com']
 	#爬取页面地址
 	start_urls = ['http://cs.ganji.com/fang1/']
 	#解析数据
 
-	def parse(self,response):
+	def parse(self, response):
 		selector = scrapy.Selector(response)
-		lenth = len(selector)
 		items = []
 		item = ZufangItem()
-		# description_name = ''
-		# address_name = ''
-		for info in selector.xpath("//div[@class='f-list-item ershoufang-list']"):
+		address_info = ''
+		# description_info = ''
+		for info in selector.xpath('//div[@class="f-list-item ershoufang-list"]'):
 			#租房标题
-			item['title'] = info.xpath(" .//dl/dd[1]/a/text()").extract()
+			item['title'] = info.xpath('./dl/dd[1]/a/text()').extract()[0]
 			#价格
-			item['money'] = info.xpath(".//dl/dd[5]/div[1]/span[1]/text()").extract()
+			item['money'] = info.xpath('./dl/dd[5]/div[1]/span[1]/text()').extract()[0]
 			#地址
-			address_info =info.xpath(".//dl/dd[3]/span")
-			item['address'] =address_info[0].xpath('string(.)').strip()
-			#房屋描述
-			description_info = info.xpath(".//dl/dd[2]/span[position()>1]/text()").extract()
-			item['description'] = description_info[0].xpath('string(.)').strip()
+			address = info.xpath('./dl/dd[3]/span/a/text()').extract()
+			# for adds in address:
+			# 	address_info += adds.strip()
+			item['address'] = ','.join(address)
+			print(address)
+			#房屋信息描述
+			description = info.xpath('./dl/dd[2]/span[position()>1]/text()').extract()
+			# for desc in description:
+				# description_info += desc.strip()
+			item['description'] = ','.join(description)
+			print(description)
 			#租房类型
-			item['type_'] = info.xpath(".//dl/dd[2]/span[1]/text()").extract()
+			item['typelist'] = info.xpath('./dl/dd[2]/span[1]/text()').extract()[0]
 			#图片地址
-			item['img'] = info.xpath(".//dl/dt/div/a/img/@src").extract()
-
+			item['img'] = info.xpath('./dl/dt/div/a/img/@src').extract()[0]
 			items.append(item)
 			yield item
 		# #翻页
